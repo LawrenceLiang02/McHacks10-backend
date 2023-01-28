53b5aa2396fd6bfcf084da26a866ee2db2730dd0
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2023-01-28 14:46:22 -0500

    added google vision

--- BackendApi.py
+++ BackendApi.py
@@ -2,8 +2,14 @@ from flask_cors import CORS
 import requests
 from flask import Flask, request, jsonify, make_response
 from decouple import config
+import io
+import os
+from google.cloud import vision
 
 app = Flask(__name__)
+
+# Imports the Google Cloud client library
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'cred.json'
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -14,9 +20,13 @@ def hello_world():
 
 @app.route('/search', methods=['GET', 'POST'])
 def select_all_from_db():
-    query = request.json["query"]
-    data = SearchCalorieNinja(query)
-    return jsonify(data)
+    query = ReadLabel()
+    labels = []
+    for label in query:
+        labels.append(SearchCalorieNinja(label.description))
+    # query = request.json["query"]
+    # data = SearchCalorieNinja(query)
+    return jsonify(labels)
 
 
 def SearchCalorieNinja(query):
@@ -27,4 +37,27 @@ def SearchCalorieNinja(query):
     else:
         print("Error:", response.status_code, response.text)
 
+
+def ReadLabel():
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath('.\Images\Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+    return labels
 

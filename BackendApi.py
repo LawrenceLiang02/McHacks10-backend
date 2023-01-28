55c293bc33a4fd7ffeaf0169473c4ae661a6c9f9
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2023-01-28 15:30:36 -0500

    json format fixed

--- BackendApi.py
+++ BackendApi.py
@@ -4,9 +4,11 @@ from flask import Flask, request, jsonify, make_response
 from decouple import config
 import io
 import os
+import json
 from google.cloud import vision
 
 app = Flask(__name__)
+app.json_encoder  
 
 # Imports the Google Cloud client library
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'cred.json'
@@ -19,11 +21,15 @@ def hello_world():
 
 
 @app.route('/search', methods=['GET', 'POST'])
-def select_all_from_db():
-    query = ReadLabel()
+def searchCommand():
+    path = '.\Images\Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg'
+    query = []
+    query = ReadLabel(path)
     labels = []
     for label in query:
-        labels.append(SearchCalorieNinja(label.description))
+        labels.append(json.loads(SearchCalorieNinja(label.description)))
+
+    print(jsonify(labels))
     # query = request.json["query"]
     # data = SearchCalorieNinja(query)
     return jsonify(labels)
@@ -33,17 +39,18 @@ def SearchCalorieNinja(query):
     api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
     response = requests.get(api_url + query, headers={'X-Api-Key': config('CalorieNinja')})
     if response.status_code == requests.codes.ok:
+        print(response.text)
         return(response.text)
     else:
         print("Error:", response.status_code, response.text)
 
 
-def ReadLabel():
+def ReadLabel(path):
     # Instantiates a client
     client = vision.ImageAnnotatorClient()
 
     # The name of the image file to annotate
-    file_name = os.path.abspath('.\Images\Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg')
+    file_name = os.path.abspath(path)
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
@@ -55,9 +62,9 @@ def ReadLabel():
     response = client.label_detection(image=image)
     labels = response.label_annotations
 
-    print('Labels:')
-    for label in labels:
-        print(label.description)
+    # print('Labels:')
+    # for label in labels:
+    #     print(label.description)
 
     return labels
 

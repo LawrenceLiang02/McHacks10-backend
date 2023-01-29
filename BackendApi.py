c865cedd8cae5cc244972a5e4306e9d91e6add8c
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2023-01-28 20:52:02 -0500

    check empty json

--- BackendApi.py
+++ BackendApi.py
@@ -1,37 +1,55 @@
 from flask_cors import CORS
 import requests
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, flash, request, jsonify, make_response
 from decouple import config
 import io
 import os
 import json
 from google.cloud import vision
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'Images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.json_encoder  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Imports the Google Cloud client library
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'cred.json'
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search', methods=['POST'])
 def searchCommand():
-    path = '.\Images\Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg'
+    file = request.files.get('file', None)
+    print(file.filename)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # print("saved")
+
     query = []
-    query = ReadLabel(path)
+    query = ReadLabel(".\Images\my-photo.png")
     labels = []
     for label in query:
-        labels.append(json.loads(SearchCalorieNinja(label.description)))
+        food = SearchCalorieNinja(label.description)
+        if (food == '{"items": []}'):
+            print("Empty")
+        else:
+            labels.append(json.loads(SearchCalorieNinja(label.description)))
 
-    # print(jsonify(labels))
-    # query = request.json["query"]
-    # data = SearchCalorieNinja(query)
+
     return jsonify(labels)
 
 
@@ -39,7 +57,7 @@ def SearchCalorieNinja(query):
     api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
     response = requests.get(api_url + query, headers={'X-Api-Key': config('CalorieNinja')})
     if response.status_code == requests.codes.ok:
-        print(response.text)
+        # print(response.text)
         return(response.text)
     else:
         print("Error:", response.status_code, response.text)
